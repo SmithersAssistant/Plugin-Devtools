f997a165fefc7de8e4a0a42190bcaa848b9fb98f
Robin Malfait <malfait.robin@gmail.com>
2016-08-07 20:21:49 +0200

    init

---
+++ dist/index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _electron = require('electron');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = _electron.remote.app;
+var getCurrentWindow = _electron.remote.getCurrentWindow;
+
+exports.default = function (robot) {
+
+  robot.listen(/^restart$/, {
+    description: "Restart",
+    usage: 'restart'
+  }, function () {
+    restart();
+  });
+
+  robot.listen(/^open devtools$/, {
+    description: 'Open the chrome dev tools',
+    usage: 'open devtools'
+  }, function () {
+    getCurrentWindow().openDevTools();
+  });
+
+  robot.listen(/^close devtools$/, {
+    description: 'Close the chrome dev tools',
+    usage: 'close devtools'
+  }, function () {
+    getCurrentWindow().closeDevTools();
+  });
+
+  robot.listen(/^toggle devtools$/, {
+    description: 'Toggle the chrome dev tools',
+    usage: 'toggle devtools'
+  }, function () {
+    getCurrentWindow().toggleDevTools();
+  });
+
+  robot.listen(/^quit$/, {
+    description: 'Close the application',
+    usage: 'quit'
+  }, function () {
+    app.quit();
+  });
+
+  robot.listen(/^refresh$/, {
+    description: 'Refresh the window',
+    usage: 'refresh'
+  }, function () {
+    window.location.reload();
+  });
+};
---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import {remote} from 'electron'
+const {app, getCurrentWindow} = remote
+
+export default robot => {
+
+  robot.listen(/^restart$/, {
+    description: "Restart",
+    usage: 'restart'
+  }, () => {
+    restart();
+  });
+
+  robot.listen(/^open devtools$/, {
+    description: 'Open the chrome dev tools',
+    usage: 'open devtools'
+  }, () => {
+    getCurrentWindow().openDevTools();
+  });
+
+  robot.listen(/^close devtools$/, {
+    description: 'Close the chrome dev tools',
+    usage: 'close devtools'
+  }, () => {
+    getCurrentWindow().closeDevTools();
+  });
+
+  robot.listen(/^toggle devtools$/, {
+    description: 'Toggle the chrome dev tools',
+    usage: 'toggle devtools'
+  }, () => {
+    getCurrentWindow().toggleDevTools();
+  });
+
+  robot.listen(/^quit$/, {
+    description: 'Close the application',
+    usage: 'quit'
+  }, () => {
+    app.quit();
+  });
+
+  robot.listen(/^refresh$/, {
+    description: 'Refresh the window',
+    usage: 'refresh'
+  }, () => {
+    window.location.reload();
+  });
+}
